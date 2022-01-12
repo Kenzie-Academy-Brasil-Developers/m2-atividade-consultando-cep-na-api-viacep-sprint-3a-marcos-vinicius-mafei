9aa1f56c45a0585a9f84283bad6309a74e3120d0
Marcos <marcosmafeimachado@gmail.com>
2022-01-12 11:41:05 -0300

    finished

--- src/js/classes.js
+++ src/js/classes.js
@@ -1,23 +1,29 @@
 
-const span = document.querySelector('#result')
+const div = document.querySelector('#result')
 
 class busca{
     constructor(){}
     async search(e){
-        span.innerHTML = ''
+        div.innerHTML = ''
         e.preventDefault();
         const input = document.querySelector('#cep-input')
         await fetch(`https://viacep.com.br/ws/${input.value}/json/unicode/`)
         .then(res => res.json())
         .then(res=>{
             if(res.cep !== undefined){
-                span.innerText = `Resultados: Rua: ${res.logradouro}, Bairro: ${res.bairro}, ${res.localidade} - ${res.uf}, DDD: ${res.ddd}.`
+                for( let i in res){
+                    if(res[i]!==''){
+                    const span = document.createElement('span')
+                    span.innerText =`${i}: ${res[i]}`
+                    div.appendChild(span)
+                    }
+                }
             }else{
-                span.innerText = `Resultados: Cep não reconhecido...`
+                div.innerText = `Resultados: Cep não reconhecido...`
             }
         })
         .catch((e)=>{
-            span.innerText = `Resultados: Verifique o Cep selecionado. Lembre-se de que o cep completo possui 8 dígitos.`
+            div.innerText = `Resultados: Verifique o Cep selecionado. Lembre-se de que o cep completo possui 8 dígitos.`
         })
     }
 }
